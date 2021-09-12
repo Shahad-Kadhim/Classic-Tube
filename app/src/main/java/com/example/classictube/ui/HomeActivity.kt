1b4611b6eb3c7950f75c011930e3a69b0aa6d7a4
Shahad Kadhim <almuhandisa.sh@gmail.com>
2021-09-12 17:25:57 +0300

    create packages

--- app/src/main/java/com/example/classictube/ui/HomeActivity.kt
+++ app/src/main/java/com/example/classictube/ui/HomeActivity.kt
@@ -1,7 +1,8 @@
-package com.example.classictube
+package com.example.classictube.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.classictube.R
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
