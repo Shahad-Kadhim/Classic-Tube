f46b3d18087d77b6c7630211d3327049a51e7744
Nooralden <nooraldenakel@gmail.com>
2021-09-14 06:34:49 +0300

    rerange

--- app/src/main/java/com/example/classictube/ui/Network.kt
+++ app/src/main/java/com/example/classictube/ui/Network.kt
@@ -4,48 +4,31 @@ import android.util.Log
 import com.example.classictube.ui.model.Feed
 import com.example.classictube.ui.model.MoviesCategory
 import com.example.classictube.ui.model.MoviesItem
-import com.google.gson.GsonBuilder
+import com.google.gson.Gson
 import okhttp3.*
 import java.io.IOException
 
 class Network {
 
-    var listOfMoClasses : MutableList<MoviesCategory> = ArrayList()
-    var listOfMoDet: MutableList<MoviesItem> = ArrayList()
+    var listMoviesCategory : MutableList<MoviesCategory> = ArrayList()
+    var listMovieItem: MutableList<MoviesItem> = ArrayList()
+    val nameOfCategory : MutableList<String?> = ArrayList()
 
-    fun makeRequestUsingOkHttp() {
+    fun makeRequestUsingOkHttp(){
         val url = "https://raw.githubusercontent.com/Bareq-altaamah/mock/main/classic.json"
         val request = Request.Builder()
             .url(url)
             .build()
         val client = OkHttpClient()
-
+        val gson = Gson()
         client.newCall(request).enqueue(object : Callback {
-            override fun onFailure(call: Call, e: IOException) {
-                Log.i("MAIN_ACTIVITY", "failed ${e.message}")//////
-            }
+            override fun onFailure(call: Call, e: IOException){
+                Log.d("MAIN_ACTIVITY", "failed ${e.message}")}
             override fun onResponse(call: Call, response: Response) {
-
                 val body = response.body?.string().toString()
-                val gson = GsonBuilder().create()
-                val homeFeed = gson.fromJson(body, Feed::class.java)
-
-                for (i in 0..2){
-                    val item = homeFeed.feed?.get(i)
-                    if (item != null) {
-                        item.moviesItems?.let { listOfMoDet.addAll(it)
-                            Log.i("MOVIES_ITEMS", item.toString())///////
-                        }
-                    }
-                }
-                homeFeed.feed?.let { listOfMoClasses.addAll(it) }
-
-//                moviesDetFilter("chaplin_rink").forEach{
-//                    Log.i("FilterDet","$it")
-//                }
-//                moviesCatFilter("1910's Feature Films").forEach{
-//                    Log.i("FilterMo","$it")
-//                }
+                Log.v("MY_TAG",body)
+                val homeFeed = gson.fromJson(body , Feed::class.java)
+                homeFeed.feed?.get(2)?.let { Log.d("NOOR", it.toString()) }
             }
         })
     }
--- app/src/main/java/com/example/classictube/ui/model/MoviesItem.kt
+++ app/src/main/java/com/example/classictube/ui/model/MoviesItem.kt
@@ -4,8 +4,9 @@ package com.example.classictube.ui.model
 import android.os.Parcel
 import android.os.Parcelable
 import com.google.gson.annotations.SerializedName
+import java.util.ArrayList
 
-data class MoviesItem (
+data class MoviesItem(
     @SerializedName("art")
     val art: String?,
     @SerializedName("description")
@@ -17,32 +18,41 @@ data class MoviesItem (
     @SerializedName("id")
     val id: String?,
     @SerializedName("ratings")
-    val ratings: String?,
+    val ratings: ArrayList<String>?,
     @SerializedName("title")
     val title: String?,
     @SerializedName("url")
     val url: String?,
     @SerializedName("year")
     val year: Int?
-):Parcelable{
+):Parcelable {
     constructor(parcel: Parcel) : this(
         parcel.readString(),
         parcel.readString(),
         parcel.readString(),
         parcel.readValue(Int::class.java.classLoader) as? Int,
         parcel.readString(),
-        parcel.readString(),
+        parcel.createStringArrayList(),
         parcel.readString(),
         parcel.readString(),
         parcel.readValue(Int::class.java.classLoader) as? Int
-    )
-
-    override fun describeContents(): Int {
-        TODO("Not yet implemented")
+    ) {
     }
 
-    override fun writeToParcel(p0: Parcel?, p1: Int) {
-        TODO("Not yet implemented")
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(art)
+        parcel.writeString(description)
+        parcel.writeString(director)
+        parcel.writeValue(duration)
+        parcel.writeString(id)
+        parcel.writeStringList(ratings)
+        parcel.writeString(title)
+        parcel.writeString(url)
+        parcel.writeValue(year)
+    }
+
+    override fun describeContents(): Int {
+        return 0
     }
 
     companion object CREATOR : Parcelable.Creator<MoviesItem> {
