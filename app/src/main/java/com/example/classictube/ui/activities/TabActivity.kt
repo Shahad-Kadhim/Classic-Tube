910a8f76e3fe75c01a2bccca59c1b46607a2062d
Shahad Kadhim <almuhandisa.sh@gmail.com>
2021-09-16 01:07:59 +0300

    add code to tab activity

--- app/src/main/java/com/example/classictube/ui/activities/TabActivity.kt
+++ app/src/main/java/com/example/classictube/ui/activities/TabActivity.kt
@@ -1,7 +1,10 @@
 package com.example.classictube.ui.activities
 
 import android.view.LayoutInflater
+import androidx.fragment.app.Fragment
+import com.example.classictube.R
 import com.example.classictube.databinding.ActivityTabBinding
+import com.example.classictube.ui.fragments.HomeFragment
 
 
 class TabActivity : BaseActivity<ActivityTabBinding>() {
@@ -12,5 +15,15 @@ class TabActivity : BaseActivity<ActivityTabBinding>() {
 
     override fun addCallbacks() {   }
 
-    override fun setUp() {   }
+    override fun setUp() {
+        replaceFragment(HomeFragment())
+    }
+
+
+    //add fragment inside Tab_Activity
+    private fun replaceFragment(fragment: Fragment){
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.fragment_container, fragment).addToBackStack(null).commit()
+    }
+
 }
