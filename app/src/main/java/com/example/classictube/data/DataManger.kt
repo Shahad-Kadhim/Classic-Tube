5a93e1201bca6752851966e6bcc2aa4a75a4a1c1
Shahad Kadhim <almuhandisa.sh@gmail.com>
2021-09-15 15:46:26 +0300

    delete network

--- app/src/main/java/com/example/classictube/data/DataManger.kt
+++
@@ -1,5 +0,0 @@
-package com.example.classictube.data
-import com.example.classictube.data.response.MoviesCategory
-
-fun moviesCategoryFilter(title:String,categoryList:List<MoviesCategory>) =
-    categoryList.filter { it.title == title }.first().moviesItems
