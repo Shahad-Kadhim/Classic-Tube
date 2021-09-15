921af64e97b9e30ae48d12cb9e447a0157e8a62c
shahad tariq <shahadtariq9@gmail.com>
2021-09-16 01:59:34 +0300

    add player and resume

--- app/src/main/java/com/example/classictube/ui/activities/PlayerActivity.kt
+++ app/src/main/java/com/example/classictube/ui/activities/PlayerActivity.kt
@@ -3,6 +3,8 @@ package com.example.classictube.ui.activities
 
 import android.view.LayoutInflater
 import com.example.classictube.databinding.ActivityPlayerBinding
+import com.google.android.exoplayer2.MediaItem
+import com.google.android.exoplayer2.SimpleExoPlayer
 
 class PlayerActivity : BaseActivity<ActivityPlayerBinding>() {
 
@@ -10,7 +12,56 @@ class PlayerActivity : BaseActivity<ActivityPlayerBinding>() {
     override val bindingInflater: (LayoutInflater) -> ActivityPlayerBinding
         = ActivityPlayerBinding::inflate
 
+    lateinit var simpleExoPlayer : SimpleExoPlayer
+
+    val STREAM_URL = "http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4"
+
     override fun addCallbacks() {   }
 
     override fun setUp() {   }
+
+    private fun initializePlayer() {
+        simpleExoPlayer = SimpleExoPlayer.Builder(this).build()
+        binding!!.videoPlayer.player = simpleExoPlayer
+        val mediaItem = MediaItem.fromUri(STREAM_URL)
+        setPlayer(mediaItem)
+
+    }
+
+    private fun setPlayer(mediaItem: MediaItem) {
+        simpleExoPlayer.apply {
+            addMediaItem(mediaItem)
+            prepare()
+            playWhenReady = true
+        }
+
+    }
+
+    private fun releasePlayer() {
+        simpleExoPlayer.release()
+    }
+
+    private fun pausePlayer() {
+        simpleExoPlayer.pause()
+    }
+
+    public override fun onStart() {
+        super.onStart()
+        initializePlayer()
+    }
+
+    public override fun onResume() {
+        super.onResume()
+        pausePlayer()
+    }
+
+    public override fun onPause() {
+        super.onPause()
+        pausePlayer()
+    }
+
+    public override fun onStop() {
+        super.onStop()
+        releasePlayer()
+    }
 }
