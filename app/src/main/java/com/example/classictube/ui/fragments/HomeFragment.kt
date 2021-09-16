a119ceff7ee607b6f313c1b7a6ba41c5a80fded3
Shahad161 <shahad.hamza.014@gmail.com>
2021-09-16 21:11:05 +0300

    Correct some colors

--- app/src/main/java/com/example/classictube/ui/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/classictube/ui/fragments/HomeFragment.kt
@@ -1,8 +1,13 @@
 package com.example.classictube.ui.fragments
 
 import android.view.LayoutInflater
+import androidx.recyclerview.widget.RecyclerView
+import com.example.classictube.R
+import com.example.classictube.data.response.MoviesCategory
+import com.example.classictube.data.response.MoviesItem
 import com.example.classictube.databinding.FragmentHomeBinding
 import com.example.classictube.ui.adapters.HomeActionListener
+import com.example.classictube.ui.adapters.NestedAdapter
 
 
 class HomeFragment : BaseFragment<FragmentHomeBinding>(), HomeActionListener {
@@ -11,11 +16,166 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>(), HomeActionListener {
     override val bindingInflater: (LayoutInflater) ->
     FragmentHomeBinding = FragmentHomeBinding::inflate
 
-    override fun addCallbacks() {}
+    override fun addCallbacks() {
+        val ll = mutableListOf<List<MoviesCategory>>().apply {
+            add(
+                listOf(
+                    MoviesCategory(null, null, null,
+                        listOf(MoviesItem(null, null, null, null, null, null, null, null, null),
+                            MoviesItem(null, null, null, null, null, null, null, null, null)), "CategoryOne"),
+                    MoviesCategory(null, null, null, listOf(MoviesItem(null, null, null, null, null, null, null, null, null),
+                            MoviesItem(null, null, null, null, null, null, null, null, null)), "CategoryOne"),
+                    MoviesCategory(null, null, null,
+                        listOf(MoviesItem(null, null, null, null, null, null, null, null, null),
+                            MoviesItem(null, null, null, null, null, null, null, null, null)), "CategoryOne"),
+                    MoviesCategory(null, null, null, listOf(MoviesItem(null, null, null, null, null, null, null, null, null),
+                        MoviesItem(null, null, null, null, null, null, null, null, null)), "CategoryOne"),
+                    MoviesCategory(null, null, null,
+                        listOf(MoviesItem(null, null, null, null, null, null, null, null, null),
+                            MoviesItem(null, null, null, null, null, null, null, null, null)), "CategoryOne"),
+                    MoviesCategory(null, null, null, listOf(MoviesItem(null, null, null, null, null, null, null, null, null),
+                        MoviesItem(null, null, null, null, null, null, null, null, null)), "CategoryOne"),
+                    MoviesCategory(null, null, null,
+                        listOf(MoviesItem(null, null, null, null, null, null, null, null, null),
+                            MoviesItem(null, null, null, null, null, null, null, null, null)), "CategoryOne"),
+                    MoviesCategory(null, null, null, listOf(MoviesItem(null, null, null, null, null, null, null, null, null),
+                        MoviesItem(null, null, null, null, null, null, null, null, null)), "CategoryOne"),
+                    MoviesCategory(null, null, null,
+                        listOf(MoviesItem(null, null, null, null, null, null, null, null, null),
+                            MoviesItem(null, null, null, null, null, null, null, null, null)), "CategoryOne"),
+                    MoviesCategory(null, null, null, listOf(MoviesItem(null, null, null, null, null, null, null, null, null),
+                        MoviesItem(null, null, null, null, null, null, null, null, null)), "CategoryOne"),
+                )
+            )
+            add(
+                listOf(
+                    MoviesCategory(null,
+                        null,
+                        null,
+                        listOf(MoviesItem(null, null, null, null, null, null, null, null, null),
+                            MoviesItem(null, null, null, null, null, null, null, null, null)),
+                        "CategoryOne"),
+                    MoviesCategory(null,
+                        null,
+                        null,
+                        listOf(MoviesItem(null, null, null, null, null, null, null, null, null),
+                            MoviesItem(null, null, null, null, null, null, null, null, null)),
+                        "CategoryOne"),
+                    MoviesCategory(null,
+                        null,
+                        null,
+                        listOf(MoviesItem(null, null, null, null, null, null, null, null, null),
+                            MoviesItem(null, null, null, null, null, null, null, null, null)),
+                        "CategoryOne"), MoviesCategory(null,
+                        null,
+                        null,
+                        listOf(MoviesItem(null, null, null, null, null, null, null, null, null),
+                            MoviesItem(null, null, null, null, null, null, null, null, null)),
+                        "CategoryOne"), MoviesCategory(null,
+                        null,
+                        null,
+                        listOf(MoviesItem(null, null, null, null, null, null, null, null, null),
+                            MoviesItem(null, null, null, null, null, null, null, null, null)),
+                        "CategoryOne"), MoviesCategory(null,
+                        null,
+                        null,
+                        listOf(MoviesItem(null, null, null, null, null, null, null, null, null),
+                            MoviesItem(null, null, null, null, null, null, null, null, null)),
+                        "CategoryOne"), MoviesCategory(null,
+                        null,
+                        null,
+                        listOf(MoviesItem(null, null, null, null, null, null, null, null, null),
+                            MoviesItem(null, null, null, null, null, null, null, null, null)),
+                        "CategoryOne"),
+                )
+
+            )
+            add(
+                listOf(
+                    MoviesCategory(null,
+                        null,
+                        null,
+                        listOf(MoviesItem(null, null, null, null, null, null, null, null, null),
+                            MoviesItem(null, null, null, null, null, null, null, null, null)),
+                        "CategoryOne"),
+                    MoviesCategory(null,
+                        null,
+                        null,
+                        listOf(MoviesItem(null, null, null, null, null, null, null, null, null),
+                            MoviesItem(null, null, null, null, null, null, null, null, null)),
+                        "CategoryOne"),
+                    MoviesCategory(null,
+                        null,
+                        null,
+                        listOf(MoviesItem(null, null, null, null, null, null, null, null, null),
+                            MoviesItem(null, null, null, null, null, null, null, null, null)),
+                        "CategoryOne"), MoviesCategory(null,
+                        null,
+                        null,
+                        listOf(MoviesItem(null, null, null, null, null, null, null, null, null),
+                            MoviesItem(null, null, null, null, null, null, null, null, null)),
+                        "CategoryOne"), MoviesCategory(null,
+                        null,
+                        null,
+                        listOf(MoviesItem(null, null, null, null, null, null, null, null, null),
+                            MoviesItem(null, null, null, null, null, null, null, null, null)),
+                        "CategoryOne"), MoviesCategory(null,
+                        null,
+                        null,
+                        listOf(MoviesItem(null, null, null, null, null, null, null, null, null),
+                            MoviesItem(null, null, null, null, null, null, null, null, null)),
+                        "CategoryOne"),
+                )
+            )
+
+            add(
+                listOf(
+                    MoviesCategory(null,
+                        null,
+                        null,
+                        listOf(MoviesItem(null, null, null, null, null, null, null, null, null),
+                            MoviesItem(null, null, null, null, null, null, null, null, null)),
+                        "CategoryOne"),
+                    MoviesCategory(null,
+                        null,
+                        null,
+                        listOf(MoviesItem(null, null, null, null, null, null, null, null, null),
+                            MoviesItem(null, null, null, null, null, null, null, null, null)),
+                        "CategoryOne"),
+                    MoviesCategory(null,
+                        null,
+                        null,
+                        listOf(MoviesItem(null, null, null, null, null, null, null, null, null),
+                            MoviesItem(null, null, null, null, null, null, null, null, null)),
+                        "CategoryOne"), MoviesCategory(null,
+                        null,
+                        null,
+                        listOf(MoviesItem(null, null, null, null, null, null, null, null, null),
+                            MoviesItem(null, null, null, null, null, null, null, null, null)),
+                        "CategoryOne"), MoviesCategory(null,
+                        null,
+                        null,
+                        listOf(MoviesItem(null, null, null, null, null, null, null, null, null),
+                            MoviesItem(null, null, null, null, null, null, null, null, null)),
+                        "CategoryOne"), MoviesCategory(null,
+                        null,
+                        null,
+                        listOf(MoviesItem(null, null, null, null, null, null, null, null, null),
+                            MoviesItem(null, null, null, null, null, null, null, null, null)),
+                        "CategoryOne"),
+                )
+            )
+        }
+        val adapter = NestedAdapter(ll, this)
+        binding?.recyclerNested?.adapter = adapter
+
+
+
+    }
+
     override fun setUp() {}
-
-
 }
+
 
 
 
