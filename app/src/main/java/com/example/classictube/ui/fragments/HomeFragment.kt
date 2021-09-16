91515fb0dee7c08ab3639cbf9e297dd37352c556
Shahad161 <shahad.hamza.014@gmail.com>
2021-09-16 14:12:06 +0300

    Add save Design

--- app/src/main/java/com/example/classictube/ui/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/classictube/ui/fragments/HomeFragment.kt
@@ -1,23 +1,15 @@
 package com.example.classictube.ui.fragments
 
 import android.view.LayoutInflater
-import androidx.recyclerview.widget.RecyclerView
-import com.example.classictube.R
-import com.example.classictube.data.response.MoviesCategory
-import com.example.classictube.data.response.MoviesItem
-import com.example.classictube.databinding.FragmentCategoriesBinding
 import com.example.classictube.databinding.FragmentHomeBinding
 import com.example.classictube.ui.adapters.HomeActionListener
-import com.example.classictube.ui.adapters.NestedAdapter
-import com.mig35.carousellayoutmanager.CarouselLayoutManager
-import com.mig35.carousellayoutmanager.CarouselZoomPostLayoutListener
 
 
-class HomeFragment : BaseFragment<FragmentCategoriesBinding>(), HomeActionListener {
+class HomeFragment : BaseFragment<FragmentHomeBinding>(), HomeActionListener {
 
     override val LOG_TAG: String = "Home_Fragment"
     override val bindingInflater: (LayoutInflater) ->
-    FragmentCategoriesBinding = FragmentCategoriesBinding::inflate
+    FragmentHomeBinding = FragmentHomeBinding::inflate
 
     override fun addCallbacks() {}
     override fun setUp() {}
